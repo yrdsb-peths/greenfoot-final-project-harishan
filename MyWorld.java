843645dbbd1ebe9a7470e4ed3fddd33c295243d5
Stewart Chan <stew.chan@gmail.com>
2022-06-07 09:48:16 -0400

    fixed background tiling

--- MyWorld.java
+++ MyWorld.java
@@ -27,8 +27,14 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
+        
+        GreenfootImage bg = new GreenfootImage("images/background.jpg");
+        bg.scale(getWidth(), getHeight());
+        setBackground(bg);
+        
         getX(100);
         getY(350);
+        
         
         spawnPlayer();
         
